2e6171d0a74b5f1238f18316876db1e91044012a
viva3825 <shunya3825@gmail.com>
2021-03-08 19:07:21 +0900

    三回目のコミット

--- item.js
+++ item.js
@@ -4,8 +4,17 @@ function handleItemSubmit(event){
     const itemText = document.querySelector(".item-text")
     const li =document.createElement("li");
     li.innerText =itemText.value;
+    const del = document.createElement("span");
+    del.innerText = "✘";
+    del.onclick = deleteItem;
+    li.appendChild(del);
     const item = document.querySelector(".item");
     item.appendChild(li);
     localStorage.setItem(itemList.length,li.innerText);
+
+}
+
+function deleteItem(){
+    alert("はい");
 
 }
