5e963f863d8a6e534455f42f91f0f18c3ee47d45
viva3825 <shunya3825@gmail.com>
2021-03-09 17:33:02 +0900

    3/9の①コミットです

--- user.js
+++ user.js
@@ -2,6 +2,12 @@ function handleUserSubmit(event){
     event.preventDefault();
     const nameText = document.querySelector(".name-text");
     localStorage.setItem("name",nameText.value);
+    // const textbox =document.getElementById("input-name");
+    // const inputValue =textbox.value;
+    // console.log(textbox);
+    // // テキストボックスの値を使用し、出力メッセージ
+    // const output ="HELLO「"+ inputValue +"」";
+    // // 出力用のP要素
+    // document.getElementById("output-name").innerHTML= output;
     location.reload();
-    // console.log(nameText.value);
 }
